6fbe9f6630c4bc74cd72e35136d967b7b55956bd
run91 <996957570@qq.com>
2016-05-23 17:09:00 +0800

    remove old file

--- path.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('ROOT_PATH', dirname(__FILE__) );
-define('LIB_PATH', ROOT_PATH . '/lib');
-define('MODEL_PATH', ROOT_PATH . '/model');
-define('SERVICE_PATH', ROOT_PATH . '/service');
-define('CLIENT_DATA_PATH', ROOT_PATH . '/client_data');
-define('DEPS_PATH', ROOT_PATH . '/deps');
-?>
