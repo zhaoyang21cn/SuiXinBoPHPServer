e4faca58eb27a2bf452a50efec27d10749914236
run91 <996957570@qq.com>
2016-05-23 17:34:24 +0800

    add db config

---
+++ lib/db/DBConfig.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * DB配置
+ */
+class DBConfig
+{
+	const HOST = 'localhost';
+	const USER = 'your-user';
+	const PASSWORD = 'your-password';
+	const DATABASE = 'sxb_db';
+	const CHARSET = 'utf8mb4';
+}
+
+?>
