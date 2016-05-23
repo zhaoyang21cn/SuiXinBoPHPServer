dee12647eb8b8fd22df1d2622185d462f3d42cb2
run91 <996957570@qq.com>
2016-05-23 17:13:13 +0800

    up

---
+++ Path.php
@@ -0,0 +1,9 @@
+<?php
+
+define('ROOT_PATH', dirname(__FILE__) );
+define('LIB_PATH', ROOT_PATH . '/lib');
+define('MODEL_PATH', ROOT_PATH . '/model');
+define('SERVICE_PATH', ROOT_PATH . '/service');
+define('CLIENT_DATA_PATH', ROOT_PATH . '/client-data');
+define('DEPS_PATH', ROOT_PATH . '/deps');
+?>
