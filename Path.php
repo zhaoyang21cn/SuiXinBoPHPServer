2197242bb9f359fd5278b0c685948318aa449394
alderzhang <alderzhang@tencent.com>
2017-06-21 20:10:47 +0800

    1. 打印日志到本地log文件夹下

--- Path.php
+++ Path.php
@@ -2,6 +2,7 @@
 
 define('ROOT_PATH', dirname(__FILE__) );
 define('LIB_PATH', ROOT_PATH . '/lib');
+define('LOG_PATH', ROOT_PATH . '/log');
 define('MODEL_PATH', ROOT_PATH . '/model');
 define('SERVICE_PATH', ROOT_PATH . '/service');
 define('CLIENT_DATA_PATH', ROOT_PATH . '/client-data');
